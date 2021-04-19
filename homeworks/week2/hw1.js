29532444e5f7014689aea38f55ab7961da83470e
vi <lyongru@gmail.com>
2021-04-19 23:44:11 +0800

    finish hw1

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,15 @@
 function printStars(n) {
-
+  let result = ''
+  let stars = ''
+  let array = []
+  for (var i=1 ; i <= n ; i++ ) {
+    result += '*\n'
+  }
+  if (result.length > 60) {
+    console.log("數字大於30，拒絕輸出")
+    } else {
+    console.log(stars+result)
+    }
 }
 
-printStars(5)
+printStars(2)
