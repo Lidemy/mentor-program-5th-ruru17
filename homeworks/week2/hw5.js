1e8beb4ae577e003ff241bbe05d728977de37e9d
vi <lyongru@gmail.com>
2021-04-23 23:09:33 +0800

    作業五未寫完存檔 寫不出來 OTZ

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,12 +1,21 @@
 function join(arr, concatStr) {
-  
+  for (var i = 0 ; i <= arr.length ; i++ )
+    while ( i > 0 ) {
+    arr.splice( arr[i], 0 , concatStr );
+    return result
+    }
 }
 
-function repeat(str, times) {
-  
-}
+var result = console.log(arr.concat(concatStr));
+return result
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
-用i+1的方式放
+/*function repeat(str, times) {
+  
+}*/
+
+console.log(join(['a','b','c'], '!'));
+
+/*console.log(repeat('a', 5));
+/*用i+1的方式放
 用double 的寫法嗎?
+題目要求用return*/
