f8fb767a3f2c4de15b8e0eaf9f4be35c8f5c930c
vi <lyongru@gmail.com>
2021-04-25 16:32:13 +0800

    finish

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,14 +1,15 @@
+/*助教好, 感謝您在week1 給我的用心回饋, 
+第五題的第一個part 我真的寫不出來. 故未完成. */
 /*function join(arr, concatStr) {
+  var re = concatStr;
   for (var i = 0 ; i <= arr.length ; i++ ) {
-    if ( i % 2 === 1) {
-      /*arr.splice( arr[i], 0 , concatStr );
-      var result = arr[i] + concatStr
+    while ( i % 2 === 1) {
+      var result = arr.replace(re, concatStr);
       }
     return result
     }
 }
-
-console.log(join(['a','b','c'], '!')); */
+console.log(join(['a','b','c','d'], '!')); */
 
 
 function repeat(str, times) {
@@ -18,4 +19,4 @@ function repeat(str, times) {
   }
   return result
 }
-console.log(repeat('yoyo', 2));
+console.log(repeat('a', 3));
